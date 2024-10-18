0d0462d2b7a702366320edc755452606b53f8e72
embanhanh <dinhnhuthong59@gmail.com>
2024-10-18 10:58:08 +0700

    fix route

--- src/routes/VoucherRoute.js
+++ src/routes/VoucherRoute.js
@@ -7,6 +7,6 @@ router.get('/get/:voucherId', voucherController.getVoucherById)
 router.post('/create', authenticateToken, voucherController.createVoucher)
 router.put('/edit/:voucherId', authenticateToken, voucherController.updateVoucher)
 router.delete('/remove/:voucherId', authenticateToken, voucherController.deleteVoucher)
-router.post('/remove-many', authenticateToken, voucherController.deleteManyVoucher)
+router.post('/delete-many', authenticateToken, voucherController.deleteManyVoucher)
 router.get('/', voucherController.getVoucher)
 module.exports = router
