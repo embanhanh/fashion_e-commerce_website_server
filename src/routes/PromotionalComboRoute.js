691eaab2edce925c635e4287e416c4222d2d715b
embanhanh <dinhnhuthong59@gmail.com>
2024-10-25 17:36:55 +0700

    edit and delete promotional combo

--- src/routes/PromotionalComboRoute.js
+++ src/routes/PromotionalComboRoute.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const promotionalComboController = require('../controllers/PromotionalComboController')
 const { authenticateToken } = require('../middlewares/AuthMiddleware')
 
-router.get('/', promotionalComboController.getAllPromotionalCombos)
+router.get('/:combo_id', promotionalComboController.getPromotionalComboById)
 router.post('/create', authenticateToken, promotionalComboController.createPromotionalCombo)
+router.put('/edit/:combo_id', authenticateToken, promotionalComboController.updatePromotionalCombo)
+router.post('/delete-many', authenticateToken, promotionalComboController.deleteManyPromotionalCombos)
+router.get('/', promotionalComboController.getAllPromotionalCombos)
 
 module.exports = router
